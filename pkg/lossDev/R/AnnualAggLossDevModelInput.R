3984babff19386c75426b533d762319361c19013
cocwl (Christopher Laws) <chris_laws@ncci.com>
2009-10-29 18:55:32 +0000

    Tightened prior for skewness parameter from 0.05 to 0.1.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/lossdev@11 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/lossDev/R/AnnualAggLossDevModelInput.R
+++ pkg/lossDev/R/AnnualAggLossDevModelInput.R
@@ -226,7 +226,7 @@ setMethod(
           ans$rho.prior <- c(2,4)
 
           ans$allow.for.skew <- ifelse(object@allowForSkew, 1, 0)
-          ans$precision.for.skewness <- 5.0E-2
+          ans$precision.for.skewness <- 0.1
 
           ans$precision.for.eta.mu <- 0.1
 
