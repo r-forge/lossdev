e3ddf4843b9dc4646d6ef95fca6cda648929cf42
cocwl (Christopher Laws) <chris_laws@ncci.com>
2010-06-29 20:54:34 +0000

    Tag v0.9.3 release.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/lossdev@140 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ TAGS/v0.9.3/src/distributions/DChisqrOV.h
@@ -0,0 +1,31 @@
+#ifndef DCHISQOV_H_
+#define DCHISQOV_H_
+
+#include <BUGS/distributions/RScalarDist.h>
+
+/**
+ * @short Chi square distribution
+ * <pre>
+ * x ~ dchisq(k)
+ * f(x|k) = 2^(-k/2) * x^(k/2 - 1) * exp(-x/2) / gamma(x/2); k > 0
+ * </pre>
+ */
+class DChisqrOV : public RScalarDist {
+ public:
+  DChisqrOV();
+
+  double d(double x, std::vector<double const *> const &parameters, 
+	   bool give_log) const;
+  double p(double q, std::vector<double const *> const &parameters, bool lower,
+	   bool give_log) const;
+  double q(double p, std::vector<double const *> const &parameters, bool lower,
+	   bool log_p) const;
+  double r(std::vector<double const *> const &parameters, RNG *rng) const;
+  /**
+   * Checks that k > 0
+   */
+  bool checkParameterValue(std::vector<double const *> const &parameters) const;
+
+};
+
+#endif /* DCHISQOV_H_ */
---
+++ TAGS/v0.9.3/src/samplers/RealSlicerOV.h
@@ -0,0 +1,56 @@
+#ifndef REAL_SLICER_OV_H_
+#define REAL_SLICER_OV_H_
+
+#include <JAGS/sampler/SampleMethod.h>
+#include <JAGS/sampler/GraphView.h>
+
+class StochasticNode;
+
+
+/**
+ * Slice sampler for real-valued distributions
+ */
+class RealSlicerOV : public SampleMethod
+{
+    GraphView const *_gv;
+    unsigned int _chain;
+
+    double _probOfOverrelaxed;
+    unsigned int _endpointAccuracy;
+
+    unsigned int _maxOV;
+    double _widthOV;
+    unsigned int _iterOV;
+    double _sumdiffOV;
+
+    bool _adaptOV;
+ 
+
+
+public:
+    /**
+     * Constructor for Slice Sampler
+     * @param node Node to sample
+     * @param width Initial width of slice
+     * @param maxwidth Maximal width of slice as a multiple of the width
+     * parameter
+     * @param nburn Length of burnin
+     */
+    RealSlicerOV(GraphView const * gv, unsigned int chain);
+    double value() const;
+    void setValue(double value) const;
+    void getLimits(double *lower, double *upper) const;
+    virtual void update(RNG *rng);
+    void updateOverrelaxed(RNG *rng);
+    void updateStep(RNG *rng);
+    virtual std::string name() const;
+    static bool canSample(StochasticNode const *node);
+    virtual bool isAdaptive() const;
+    virtual bool adaptOff();
+    double logFullConditional() const;
+};
+
+
+
+#endif /* REAL_SLICER_OV_H_ */
+
