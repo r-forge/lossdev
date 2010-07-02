f44ada7336384ffadb36a20f6c712b11bdddd26b
cocwl (Christopher Laws) <chris_laws@ncci.com>
2010-07-02 15:21:35 +0000

    Including suggestions by Plummer.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/lossdev@142 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- TAGS/v0.9.3/jags-win32/include/JAGS/version.h
+++
@@ -1,8 +0,0 @@
-#ifndef JAGS_VERSION_H_
-#define JAGS_VERSION_H_
-
-extern "C" {
-   char const * jags_version();
-}
-
-#endif /* JAGS_VERSION_H_ */
--- TAGS/v0.9.3/jags-win64/include/JAGS/util/integer.h
+++
@@ -1,7 +0,0 @@
-#ifndef UTIL_INTEGER_H_
-#define UTIL_INTEGER_H_
-
-bool checkInteger(double);
-int asInteger(double);
-
-#endif /* UTIL_INTEGER_H_ */
